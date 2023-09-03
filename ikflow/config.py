7e31603769f4c5bb2c03c0c2123f981153a98dca
Jeremy Morgan <jsmorgan6@gmail.com>
2023-09-03 14:29:08 -0700

    importing DEVICE from jrl

--- ikflow/config.py
+++ ikflow/config.py
@@ -1,40 +1,11 @@
 """Global configuration"""
-import torch
 import os
-from time import sleep
+
+import torch
+from jrl.config import GPU_IDX
+from jrl.config import DEVICE as device 
 
 
-def _get_device() -> str:
-    if not torch.cuda.is_available():
-        return "cpu", -1
-
-    n_devices = torch.cuda.device_count()
-    if n_devices == 1:
-        return "cuda:0", 0
-
-    def _mem_and_utilitization_ave_usage_pct(device_idx: int):
-        device = torch.cuda.device(device_idx)
-        mems = []
-        utils = []
-        for i in range(2):
-            mems.append(torch.cuda.memory_usage(device=device))
-            utils.append(torch.cuda.utilization(device=device))
-            sleep(0.5)
-        return sum(mems) / len(mems), sum(utils) / len(utils)
-
-    min_mem = 100
-    min_util = 100
-    for i in range(n_devices):
-        mem_pct, util_pct = _mem_and_utilitization_ave_usage_pct(i)
-        min_mem = min(min_mem, mem_pct)
-        min_util = min(min_util, util_pct)
-        if mem_pct < 5.0 and util_pct < 9:
-            return f"cuda:{i}", i
-    raise EnvironmentError(f"No unused GPU's available. Minimum memory, utilization: {min_mem, min_util}%")
-
-
-# /
-device, GPU_IDX = _get_device()
 DEFAULT_TORCH_DTYPE = torch.float32
 print(f"ikflow/config.py | Using device: '{device}'")
 
